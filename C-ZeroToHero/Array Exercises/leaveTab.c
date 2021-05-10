a93b0b48e3c8bc94ac6dc1387f45751817c1c5dc
Ahmed Keçeci <37221165+ahmedkececi@users.noreply.github.com>
2021-05-10 15:13:34 +0300

    Create leaveTab.c

---
+++ C-ZeroToHero/Array Exercises/leaveTab.c
@@ -0,0 +1,19 @@
+//The program prints each word of the sentence by leaving a tab space.
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char sentence[100];
+    int i,j,counter=0;
+    printf("Enter Your Sentence : ");
+    gets(sentence);
+    for (i=0;sentence[i];i++){
+        printf("%c",sentence[i]);
+        if (sentence[i]==32) {
+            printf("\n");
+        }
+        counter++;
+        for (j=0;j<counter; j++) {
+            printf("\t");
+        }
+    }
+}
